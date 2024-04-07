0e13b6bd767db1594d06ebaaed98d553e25a2dce
Piyush <piyushpratapsingh67@gmail.com>
2024-04-07 12:56:31 +0530

    Project Added (version01)

---
+++ script.js
@@ -0,0 +1,9 @@
+function SubscribeButtonClicked() {
+    const box = document.getElementsByClassName("subscribeContent")[0];
+    box.style.visibility = "visible";
+}
+
+function onCrossClick() {
+    const box = document.getElementsByClassName("subscribeContent")[0];
+    box.style.visibility = "hidden";
+}
